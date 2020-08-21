32187c1e04fa84b2a58465156cc9e35c18d8532e
ggiles28 <gariel.lynn@gmail.com>
2020-08-21 14:08:26 -0500

    connected mongodb successfully

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
+// DOTENV =================
+require('dotenv').config()
+// DEPENDANCIES ===========
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const PORT = process.env.PORT || 3000;
+const reactViews = require('express-react-views');
+const createEngine = reactViews.createEngine
+
+// const methodOverride = require('method-override');
+
+const mongoURI = process.env.MONGO_URI;
+const db = mongoose.connection;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+db.once('open', () => {
+    console.log('Mongo is working!');
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+// app.use(methodOverride('_method'));
 
 app.listen(PORT, (req, res) => {
     console.log('Check the kicks!');
