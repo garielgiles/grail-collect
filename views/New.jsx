380c7cf671a12badae2c775d5d3dafd8c0b78078
ggiles28 <gariel.lynn@gmail.com>
2020-08-26 02:12:27 -0500

    styled New page

--- views/New.jsx
+++ views/New.jsx
@@ -1,26 +1,41 @@
 const React = require('react')
+const Default = require('./components/Default')
 
 class New extends React.Component {
     render() {
         return (
-            <div>
-                <title>Add Sneaker</title>
-                <h1>Add to Collection</h1>
-                <form action="/collection" method="POST">
-                    Name: <input type="text" name="name" /><br />
-                    Size: <input type="text" name="size" /><br />
-                    Resell: <input type="text" name="resell" /><br />
-                    Retail: <input type="text" name="retail" /><br />
-                    Deadstock: <input type="checkbox" name="deadstock"/><br />
-                    {/* Worn: <input type="checkbox" name="worn" /><br /> */}
-                    Description: <input type="text" name="description" /><br />
-                    Image: <input type="text" name="image" /><br />
-                    {/* <input type="file" id="img" name="image" accept="image/*" /><br /> */}
-                    {/* <label htmlFor="image">Select image:</label>
+            <Default>
+                <body className="pageBody">
+
+
+                    <div className="">
+                        <title>Add Sneaker</title>
+                        <div>
+                            <nav class="navbar fixed-top navbar-light bg-light">
+                                <span class="navbar-brand mb-0 h1" id="navFont">Grail Collect</span>
+                            </nav>
+                        </div>
+                        <h1 className="pageFont">Add to Collection</h1>
+                        <div className="genBody">
+                        <form action="/collection" method="POST">
+                            Name: <input type="text" name="name" /><br />
+                            Size: <input type="text" name="size" /><br />
+                            Resell: <input type="text" name="resell" /><br />
+                            Retail: <input type="text" name="retail" /><br />
+                            Deadstock: <input type="checkbox" name="deadstock" /><br />
+                            {/* Worn: <input type="checkbox" name="worn" /><br /> */}
+                            Description: <input className="des" type="text" name="description" /><br />
+                            Image: <input type="text" name="image" /><br />
+                            {/* <input type="file" id="img" name="image" accept="image/*" /><br /> */}
+                            {/* <label htmlFor="image">Select image:</label>
                     <input type="file" id="img" name="img" accept="image/*"/><br /> */}
-                    <input type="submit" name="" value="Create Sneaker" />
-                </form>
-            </div>
+                            <input class="btn btn-success btn-sm" type="submit" name="" value="Create Sneaker" />
+                        </form>
+                        </div>
+                    </div>
+
+                </body>
+            </Default>
         )
     }
 }
